1c3ada1d29807d0f3d81747433c3a4c4fa1b7cc0
osmihi <othman@osmihi.com>
2013-06-22 23:55:42 -0500

    Got the mod_rewrite rule working

--- api/index.php
+++ api/index.php
@@ -1,7 +1,16 @@
 <html>
 	<body>
 		<?php 
-			echo "api";
+			$response = "";
+			$response .= "<html>" . PHP_EOL;
+			$response .= "\t<body>" .PHP_EOL;
+			$response .= "\t\t<p>" . PHP_EOL . "\t\t\t" . $_SERVER['REQUEST_URI'] . PHP_EOL . "\t\t</p>" . PHP_EOL;
+			$response .= "\t\t<p>" . PHP_EOL . "\t\t\t";
+			var_dump($_GET);
+			$response .= PHP_EOL . "\t\t</p>" . PHP_EOL;
+			$response .= "\t</body>" . PHP_EOL;
+			$response .= "</html>" . PHP_EOL;
+			echo $response;
 		?>
 	</body>
 </html>
